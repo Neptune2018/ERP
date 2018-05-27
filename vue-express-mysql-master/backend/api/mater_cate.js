fa57583e562cc290ba6cebce4fc78f8ef2203457
TotPil <846685686@qq.com>
2018-05-27 19:09:13 +0900

    add 3 test functions

--- vue-express-mysql-master/backend/api/mater_cate.js
+++ vue-express-mysql-master/backend/api/mater_cate.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var Mater_cate = require('../proxy').Mater_cate;
 
-//列出所有物料分类，当然只列出根类，不会列出子类
+//列出所有物料分类，当然只列出顶级分类，不会列出子类
 exports.listallMater_cates = function(req, res) {
 	console.log(req.session.sess)
 	req.session.sess = 'yes';
--- vue-express-mysql-master/backend/proxy/mater_cate.js
+++ vue-express-mysql-master/backend/proxy/mater_cate.js
@@ -24,6 +24,19 @@ exports.listallMater_cates = function(callback) {
     );
 };
 
+// 查找没有父类的物料分类,也就是最高层分类
+exports.listallMater_cates = function(callback) {
+    MaterCate.findAll({
+    'where': {
+        'materCateId': null
+    }
+}).then(
+        function(result){
+            callback(result);
+        }
+    );
+};
+
 
 // 查找指定父类的物料分类.注意：虽然不知道前端怎么显示，物料分类和具体的物料是分开查找的
 exports.listallMater_catesbyMaterCateID = function (materCateId,callback) {
