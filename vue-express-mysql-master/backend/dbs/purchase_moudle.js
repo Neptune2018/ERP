2e6412954ff94d3784225754238a62c5b6bcd76e
huangyongke <1773877625@qq.com>
2018-05-31 23:40:52 +0800

    HYK,add database for supplier,#59

--- vue-express-mysql-master/backend/dbs/purchase_moudle.js
+++ vue-express-mysql-master/backend/dbs/purchase_moudle.js
@@ -4,9 +4,11 @@ var models = require('../models')
 //在当前文件夹中 采用命令 node role_feature.js就能把权限数据插入到数据表中
 
 const addSupplier = async function () {
-//      await models.Material.destroy({where:{}});
-//    	await models.MaterCate.destroy({where:{}});
-//    	await models.Stock.destroy({where:{}});
+    await models.MinOrder.destroy({where:{}});
+    await models.MaterCate.destroy({where:{}});
+    await models.Material.destroy({where:{}});
+    await models.Stock.destroy({where:{}});
+    await models.Supplier.destroy({where:{}});
 
     var material1 = await models.Material.create({
         'name': 'EB38 PCB上线',
@@ -139,6 +141,66 @@ const addSupplier = async function () {
     material4.addStock(stock6)
     material5.addStock(stock6)
 
+    var Supplier1 = await models.Supplier.create({
+		'name': '众汇有限责任公司',
+        'phone': '12334445678',
+        'person':'张三',
+        'remark': 'xx原料供应商'
+	});
+    var Supplier2 = await models.Supplier.create({
+		'name': '阳驰有限责任公司',
+        'phone': '12332344223',
+        'person':'李四',
+        'remark': 'xx原料供应商'
+	});
+	var Supplier3 = await models.Supplier.create({
+		'name': '卓宇有限责任公司',
+        'phone': '12344442378',
+        'person':'王五',
+        'remark': 'xx原料供应商'
+	});
+    var Supplier4 = await models.Supplier.create({
+		'name': '丹文有限责任公司',
+        'phone': '12323446789',
+        'person':'小强',
+        'remark': 'xx原料供应商'
+    });
+    var Supplier5 = await models.Supplier.create({
+		'name': '高德有限责任公司',
+        'phone': '12323234456',
+        'person':'周鸿煊',
+        'remark': 'xx原料供应商'
+    });
+    var Supplier6 = await models.Supplier.create({
+		'name': '智宸有限责任公司',
+        'phone': '12323489076',
+        'person':'李立诚',
+        'remark': 'xx原料供应商'
+    });
+    var Supplier7 = await models.Supplier.create({
+		'name': '弘文有限责任公司',
+        'phone': '12323445678',
+        'person':'张弘文',
+        'remark': 'xx原料供应商'
+    });
+    
+    material1.addSupplier(Supplier1, {'quantity': 100})
+    material1.addSupplier(Supplier2, {'quantity': 400})
+    material1.addSupplier(Supplier3, {'quantity': 500})
+    material2.addSupplier(Supplier4, {'quantity': 400})
+    material2.addSupplier(Supplier5, {'quantity': 400})
+    material2.addSupplier(Supplier6, {'quantity': 700})
+    material3.addSupplier(Supplier7, {'quantity': 400})
+    material3.addSupplier(Supplier3, {'quantity': 500})
+    material3.addSupplier(Supplier4, {'quantity': 300})
+    material4.addSupplier(Supplier4, {'quantity': 200})
+    material4.addSupplier(Supplier7, {'quantity': 600})
+    material5.addSupplier(Supplier1, {'quantity': 200})
+    material5.addSupplier(Supplier3, {'quantity': 400})
+    material5.addSupplier(Supplier5, {'quantity': 300})
+    material6.addSupplier(Supplier2, {'quantity': 100})
+    material6.addSupplier(Supplier3, {'quantity': 600})
+    material6.addSupplier(Supplier6, {'quantity': 300})
 
 
 }
