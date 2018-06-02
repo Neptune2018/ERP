817daf6b1dfe0385c5cd4ad173c209ea9158e4a0
huangyongke <1773877625@qq.com>
2018-06-02 12:41:35 +0800

    HYK,add data to offerlist,#72

--- vue-express-mysql-master/backend/dbs/purchase_moudle.js
+++ vue-express-mysql-master/backend/dbs/purchase_moudle.js
@@ -6,9 +6,11 @@ var models = require('../models')
 const addSupplier = async function () {
     await models.MinOrder.destroy({where:{}});
     await models.MaterCate.destroy({where:{}});
-    await models.Material.destroy({where:{}});
     await models.Stock.destroy({where:{}});
+    await models.Material.destroy({where:{}});
+    await models.Offer.destroy({where:{}});
     await models.Supplier.destroy({where:{}});
+    await models.OfferList.destroy({where:{}});
 
     var material1 = await models.Material.create({
         'name': 'EB38 PCB上线',
@@ -122,24 +124,24 @@ const addSupplier = async function () {
     });
 
 
-    materialcate1.addMaterial(material1)
-    materialcate1.addMaterial(material3)
-    materialcate1.addMaterial(material5)
-    materialcate2.addMaterial(material2)
-    materialcate2.addMaterial(material2)
-    materialcate3.addMaterial(material4)
-    materialcate3.addMaterial(material6)
+    await materialcate1.addMaterial(material1)
+    await materialcate1.addMaterial(material3)
+    await materialcate1.addMaterial(material5)
+    await materialcate2.addMaterial(material2)
+    await materialcate2.addMaterial(material2)
+    await materialcate3.addMaterial(material4)
+    await materialcate3.addMaterial(material6)
 
-    material1.addStock(stock1)
-    material1.addStock(stock2)
-    material1.addStock(stock3)
-    material3.addStock(stock4)
-    material4.addStock(stock5)
-    material4.addStock(stock6)
-    material5.addStock(stock6)
-    material6.addStock(stock7)
-    material4.addStock(stock6)
-    material5.addStock(stock6)
+    await material1.addStock(stock1)
+    await material1.addStock(stock2)
+    await material1.addStock(stock3)
+    await material3.addStock(stock4)
+    await material4.addStock(stock5)
+    await material4.addStock(stock6)
+    await material5.addStock(stock6)
+    await material6.addStock(stock7)
+    await material4.addStock(stock6)
+    await material5.addStock(stock6)
 
     var Supplier1 = await models.Supplier.create({
 		'name': '众汇有限责任公司',
@@ -184,24 +186,127 @@ const addSupplier = async function () {
         'remark': 'xx原料供应商'
     });
     
-    material1.addSupplier(Supplier1, {'quantity': 100})
-    material1.addSupplier(Supplier2, {'quantity': 400})
-    material1.addSupplier(Supplier3, {'quantity': 500})
-    material2.addSupplier(Supplier4, {'quantity': 400})
-    material2.addSupplier(Supplier5, {'quantity': 400})
-    material2.addSupplier(Supplier6, {'quantity': 700})
-    material3.addSupplier(Supplier7, {'quantity': 400})
-    material3.addSupplier(Supplier3, {'quantity': 500})
-    material3.addSupplier(Supplier4, {'quantity': 300})
-    material4.addSupplier(Supplier4, {'quantity': 200})
-    material4.addSupplier(Supplier7, {'quantity': 600})
-    material5.addSupplier(Supplier1, {'quantity': 200})
-    material5.addSupplier(Supplier3, {'quantity': 400})
-    material5.addSupplier(Supplier5, {'quantity': 300})
-    material6.addSupplier(Supplier2, {'quantity': 100})
-    material6.addSupplier(Supplier3, {'quantity': 600})
-    material6.addSupplier(Supplier6, {'quantity': 300})
+    
+    await material1.addSupplier(Supplier1, {'quantity' : 100})
+    await material1.addSupplier(Supplier2, {'quantity' : 400})
+    await material1.addSupplier(Supplier3, {'quantity' : 500})
+    await material2.addSupplier(Supplier4, {'quantity' : 400})
+    await material2.addSupplier(Supplier5, {'quantity' : 400})
+    await material2.addSupplier(Supplier6, {'quantity' : 700})
+    await material3.addSupplier(Supplier7, {'quantity' : 400})
+    await material3.addSupplier(Supplier3, {'quantity' : 500})
+    await material3.addSupplier(Supplier4, {'quantity' : 300})
+    await material4.addSupplier(Supplier4, {'quantity' : 200})
+    await material4.addSupplier(Supplier7, {'quantity' : 600})
+    await material5.addSupplier(Supplier1, {'quantity' : 200})
+    await material5.addSupplier(Supplier3, {'quantity' : 400})
+    await material5.addSupplier(Supplier5, {'quantity' : 300})
+    await material6.addSupplier(Supplier2, {'quantity' : 100})
+    await material6.addSupplier(Supplier3, {'quantity' : 600})
+    await material6.addSupplier(Supplier6, {'quantity' : 300})
 
+    var offerList1 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList2 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList3 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList4 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList5 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList6 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList7 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList8 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList9 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList10 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList11 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList12 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList13 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList14 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList15 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList16 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList17 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList18 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+    var offerList19 = await models.OfferList.create({
+        'time': '2018-02-15 12:00:00'
+    });
+
+    Supplier1.addOfferList(offerList1)
+    Supplier1.addOfferList(offerList2)
+    Supplier1.addOfferList(offerList3)
+    Supplier2.addOfferList(offerList4)
+    Supplier2.addOfferList(offerList5)
+    Supplier3.addOfferList(offerList6)
+    Supplier4.addOfferList(offerList7)
+    Supplier4.addOfferList(offerList8)
+    Supplier4.addOfferList(offerList9)
+    Supplier4.addOfferList(offerList10)
+    Supplier5.addOfferList(offerList11)
+    Supplier5.addOfferList(offerList12)
+    Supplier6.addOfferList(offerList13)
+    Supplier6.addOfferList(offerList14)
+    Supplier6.addOfferList(offerList15)
+    Supplier7.addOfferList(offerList16)
+    Supplier7.addOfferList(offerList17)
+    Supplier7.addOfferList(offerList18)
+    Supplier7.addOfferList(offerList19)
+
+    offerList1.addMaterial(material1,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList1.addMaterial(material5,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList2.addMaterial(material1,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList2.addMaterial(material5,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList3.addMaterial(material1,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList3.addMaterial(material5,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList4.addMaterial(material1,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList4.addMaterial(material6,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList5.addMaterial(material1,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList6.addMaterial(material5,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList7.addMaterial(material2,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList8.addMaterial(material3,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList9.addMaterial(material4,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList10.addMaterial(material2,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList11.addMaterial(material2,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList12.addMaterial(material5,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList13.addMaterial(material2,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList13.addMaterial(material2,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList14.addMaterial(material6,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList15.addMaterial(material6,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList16.addMaterial(material3,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList17.addMaterial(material4,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList18.addMaterial(material3,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
+    offerList19.addMaterial(material4,{'quantity': 100, 'batch': '1-2', 'price': 20, 'total_price': 2000})
 
 }
 
